8eb0da05af8a1e8b64d1c3e4dfff6c6e3a6ed196
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2026-09-17 18:09:14 +0300

    rdma-core 65.0 (#4)

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,13 @@ export CMAKE_CONFIG="Release"
 mkdir "build_${CMAKE_CONFIG}"
 pushd "build_${CMAKE_CONFIG}"
 
+# 64.0+ installs rdma-sysusers.conf into SYSUSERS_DIR; default is /usr/lib/sysusers.d.
 cmake ${CMAKE_ARGS} \
       -G Ninja \
       -D CMAKE_BUILD_TYPE:STRING="${CMAKE_CONFIG}" \
       -D CMAKE_PREFIX_PATH:PATH="${PREFIX}" \
       -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+      -D SYSUSERS_DIR:PATH="${PREFIX}/lib/sysusers.d" \
       "${SRC_DIR}" \
 ;
 cmake --build . --parallel ${CPU_COUNT}
